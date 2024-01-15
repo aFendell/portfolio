17cf912ab71686990a25c5362eb410967b0239ee
Assaf Fendell <fendell.assaf@gmail.com>
2024-01-15 12:42:42 +0200

    feat: shadcn setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
+import { cn } from '../lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
-  icons: {},
   title: 'fendi.dev | Portfolio',
   description: 'Assaf Fendell full-stack developer portfolio.',
 };
@@ -17,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body
+        className={cn(
+          'bg-gray-50 text-gray-950 relative min-h-screen font-sans antialiased',
+          fontSans.variable
+        )}
+      >
         <div className='bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[-51rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
         {children}

fac9bc49e59d9fb90af46b66d1295341b68ceab4
Assaf Fendell <fendell.assaf@gmail.com>
2024-02-13 18:45:25 +0200

    fix: projects list gap

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
       <Muted>{subtitle}</Muted>
       <SectionTitle>{title}</SectionTitle>
 
-      <ul className="grid grid-cols-1 content-center gap-3 sm:gap-6">
+      <ul className="grid grid-cols-1 content-center gap-6 sm:gap-8">
         {projects.map((project) => (
           <li key={project.title}>
             <ProjectItem project={project} />
